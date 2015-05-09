0f4164c469adc64c5e68fca106bee9b9a623190f
Salvador Tejero <stejeros@indra.es>
2015-05-09 21:19:21 +0200

    Service creation and README.md

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# liferay-push-windowsphone
-Push Service for Windows Phone
+liferay-push-windowsphone
+======================
+
+Service for Liferay SDK Windows Phone for Push Notifications. It's an extension for [Ithildir](https://github.com/Ithildir/liferay-sdk-builder-windows) and [Bruno Farache IOS Service] (https://github.com/brunofarache/liferay-push-ios)
+
+If you find any bug, please create a Issue.
+``` 
+Liferay.connect(null, null, 'http://10.0.2.2:8080', 'test@liferay.com', 'test' );
+``` 
+## Installation
+``` 
+Copy the folder Notification and paste in Liferay Services project or folder.
+``` 
+
+### Methods
+
+- AddPushNotificationsDeviceAsync(string token, string platform)
+- DeletePushNotificationsDeviceAsync(string token)
+- SendPushNotificationWithToUserIdsAsync(IEnumerable<int> toUserIds, string payload)
+- SendPushNotificationWithPlatformAsync(string platform, IEnumerable<string> tokens, string payload)
+
+##More info:
+
+For more information read [Bruno Farache IOS Service] (https://github.com/brunofarache/liferay-push-ios)
---
+++ Service/V62/Notification/NotificationService.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liferay.SDK.Service.V62.Notification
+{
+    class NotificationService: ServiceBase
+	{
+        public NotificationService(ISession session)
+			: base(session)
+		{
+		}
+
+
+        public async Task<dynamic> AddPushNotificationsDeviceAsync(string token, string platform)
+        {
+            var _parameters = new JsonObject();
+
+            _parameters.Add("token", token);
+            _parameters.Add("platform", platform);
+
+            var _command = new JsonObject()
+			{
+				{ "/push-notifications-portlet/pushnotificationsdevice/add-push-notifications-device", _parameters }
+			};
+
+            var _obj = await this.Session.InvokeAsync(_command).ConfigureAwait(false);
+
+            return (dynamic)_obj;
+        }
+
+        public async Task<dynamic> DeletePushNotificationsDeviceAsync(string token)
+        {
+            var _parameters = new JsonObject();
+
+            _parameters.Add("token", token);
+
+            var _command = new JsonObject()
+			{
+				{ "/push-notifications-portlet/pushnotificationsdevice/delete-push-notifications-device", _parameters }
+			};
+
+            var _obj = await this.Session.InvokeAsync(_command).ConfigureAwait(false);
+
+            return (dynamic)_obj;
+        }
+
+        public async Task<dynamic> SendPushNotificationWithToUserIdsAsync(IEnumerable<int> toUserIds, string payload)
+        {
+            var _parameters = new JsonObject();
+
+            _parameters.Add("toUserIds", toUserIds);
+            _parameters.Add("payload", payload);
+
+            var _command = new JsonObject()
+			{
+				{ "/push-notifications-portlet/pushnotificationsdevice/send-push-notification", _parameters }
+			};
+
+            var _obj = await this.Session.InvokeAsync(_command).ConfigureAwait(false);
+
+            return (dynamic)_obj;
+        }
+
+        public async Task<dynamic> SendPushNotificationWithPlatformAsync(string platform, IEnumerable<string> tokens, string payload)
+        {
+            var _parameters = new JsonObject();
+
+            _parameters.Add("platform", platform);
+            _parameters.Add("tokens", tokens);
+            _parameters.Add("payload", payload);
+
+            var _command = new JsonObject()
+			{
+				{ "/push-notifications-portlet/pushnotificationsdevice/send-push-notification", _parameters }
+			};
+
+            var _obj = await this.Session.InvokeAsync(_command).ConfigureAwait(false);
+
+            return (dynamic)_obj;
+        }
+    }
+}
